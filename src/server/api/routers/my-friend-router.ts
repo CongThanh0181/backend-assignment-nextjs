d4166ef149b753df97c5f633dd7dcb6b69ad1e1f
thanh cong luu <luuthanhcong1810@gmail.com>
2024-08-23 00:31:05 +0700

    Finish assignment

---
+++ src/server/api/routers/my-friend-router.ts
@@ -0,0 +1,72 @@
+import type { Database } from '@/server/db'
+
+import { TRPCError } from '@trpc/server'
+import { z } from 'zod'
+
+import { FriendshipStatusSchema } from '@/utils/server/friendship-schemas'
+import { protectedProcedure } from '@/server/trpc/procedures'
+import { router } from '@/server/trpc/router'
+import { IdSchema } from '@/utils/server/base-schemas'
+
+export const myFriendRouter = router({
+  getById: protectedProcedure
+    .input(
+      z.object({
+        friendUserId: IdSchema,
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      return ctx.db.connection().execute(async (conn) => {
+        const mutualFriendCountQuery = await conn
+          .selectFrom('friendships as f1')
+          .innerJoin('friendships as f2', 'f1.friendUserId', 'f2.friendUserId')
+          .where('f1.userId', '=', ctx.session.userId)
+          .where('f2.userId', '=', input.friendUserId)
+          .where('f1.status', '=', FriendshipStatusSchema.Values['accepted'])
+          .where('f2.status', '=', FriendshipStatusSchema.Values['accepted'])
+          .select((eb) => [
+            eb.fn.count('f1.friendUserId').as('mutualFriendCount'),
+          ])
+          .executeTakeFirstOrThrow(() => new TRPCError({ code: 'NOT_FOUND' }))
+
+        const friendInfo = await conn
+          .selectFrom('users as friends')
+          .innerJoin('friendships', 'friendships.friendUserId', 'friends.id')
+          .innerJoin(
+            userTotalFriendCount(conn).as('userTotalFriendCount'),
+            'userTotalFriendCount.userId',
+            'friends.id'
+          )
+          .where('friendships.userId', '=', ctx.session.userId)
+          .where('friendships.friendUserId', '=', input.friendUserId)
+          .where(
+            'friendships.status',
+            '=',
+            FriendshipStatusSchema.Values['accepted']
+          )
+          .select([
+            'friends.id',
+            'friends.fullName',
+            'friends.phoneNumber',
+            'userTotalFriendCount.totalFriendCount',
+          ])
+          .executeTakeFirstOrThrow(() => new TRPCError({ code: 'NOT_FOUND' }))
+
+        return {
+          ...friendInfo,
+          mutualFriendCount: mutualFriendCountQuery.mutualFriendCount,
+        }
+      })
+    }),
+})
+
+const userTotalFriendCount = (db: Database) => {
+  return db
+    .selectFrom('friendships')
+    .where('friendships.status', '=', FriendshipStatusSchema.Values['accepted'])
+    .select((eb) => [
+      'friendships.userId',
+      eb.fn.count('friendships.friendUserId').as('totalFriendCount'),
+    ])
+    .groupBy('friendships.userId')
+}
